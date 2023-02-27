60f426457b580ab26e569dc805cad1ed99d2beaf
ansehyeong <dkstpgud@gmail.com>
2023-02-27 16:50:11 +0900

    feat: initial commit

---
+++ src/test/java/org/moderd/moderdspringautoconfigure/ModerdSpringAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moderd.moderdspringautoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerdSpringAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
